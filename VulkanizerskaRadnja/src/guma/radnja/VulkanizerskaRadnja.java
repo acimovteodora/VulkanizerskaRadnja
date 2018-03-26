deea3db36f4e4d44dfe0df768a202fec3be537c8
Teodora Acimov <acimov.teodora@gmail.com>
2018-03-26 22:41:45 +0200

    Napravljena klasa VulkanizerskaRadnja u paketu guma.radnja

---
+++ VulkanizerskaRadnja/src/guma/radnja/VulkanizerskaRadnja.java
@@ -0,0 +1,33 @@
+package guma.radnja;
+
+import java.util.LinkedList;
+
+import guma.AutoGuma;
+
+public class VulkanizerskaRadnja {
+	
+	private LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
+
+	public void dodajGumu(AutoGuma a) {
+		
+		if (a == null)
+			throw new NullPointerException("Guma ne sme biti null");
+		
+		if (gume.contains(a))
+			throw new RuntimeException("Guma vec postoji");
+		
+		gume.addFirst(a);
+	}
+
+	public LinkedList<AutoGuma> pronadjiGumu(String markaModel) {
+		if (markaModel == null)
+			return null;
+		
+		LinkedList<AutoGuma> novaLista = new LinkedList<AutoGuma>();
+		for (int i = 0; i < gume.size(); i++)
+			if (gume.get(i).getMarkaModel().equals(markaModel))
+				novaLista.add(gume.get(i));
+		
+		return novaLista;
+	}
+}
